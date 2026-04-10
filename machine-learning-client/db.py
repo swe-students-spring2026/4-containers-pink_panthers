2e652d335511a26ac3f306aedaac4cbcf315886c
lilylorand <lblorand@gmail.com>
2026-04-10 19:38:10 -0400

    fix pylint and pymongo setup

--- machine-learning-client/db.py
+++ machine-learning-client/db.py
@@ -8,7 +8,9 @@ db = client["outfit_db"]
 collection = db["results"]
 
 def insert_result(data):
+    """Insert a document into MongoDB."""
     collection.insert_one(data)
 
 def get_latest():
+    """Retrieve the latest document from MongoDB."""
     return collection.find_one(sort=[("_id", -1)])
