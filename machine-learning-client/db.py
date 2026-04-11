c760b4732ba31bf60dcf6ca4f9139c1b9a9cf7f0
lilylorand <lblorand@gmail.com>
2026-04-10 20:00:21 -0400

    format machine learning client with black

--- machine-learning-client/db.py
+++ machine-learning-client/db.py
@@ -1,15 +1,18 @@
 """
 Database module for MongoDB operations.
 """
+
 import pymongo
 
 client = pymongo.MongoClient("mongodb://localhost:27017/")
 db = client["outfit_db"]
 collection = db["results"]
 
+
 def insert_result(data):
     """Insert a document into MongoDB."""
     collection.insert_one(data)
+
 
 def get_latest():
     """Retrieve the latest document from MongoDB."""
