be27273a7dad53812ff06a5fd9cdef94525a6b58
SunilParab <sunilparab2023@gmail.com>
2026-04-13 21:56:55 -0400

    Fix for tests and pipfile

--- machine-learning-client/tests/test_model.py
+++ machine-learning-client/tests/test_model.py
@@ -5,8 +5,11 @@ from tests.fake_collection import FakeCollection
 
 def test_prediction_runs():
     fake_training = FakeCollection()
+    fake_results = FakeCollection()
 
-    model = OutfitModel(training_collection=fake_training)
+    model = OutfitModel(
+        training_collection=fake_training, results_collection=fake_results
+    )
 
     model.model.fit([[255, 0, 0, 0, 0, 0]], [1.0])
     model.trained = True
@@ -18,8 +21,11 @@ def test_prediction_runs():
 
 def test_model_requires_training():
     fake_training = FakeCollection()
+    fake_results = FakeCollection()
 
-    model = OutfitModel(training_collection=fake_training)
+    model = OutfitModel(
+        training_collection=fake_training, results_collection=fake_results
+    )
 
     with pytest.raises(RuntimeError):
         model.predict_score((255, 0, 0), (0, 0, 0))
@@ -27,8 +33,11 @@ def test_model_requires_training():
 
 def test_load_training_data():
     fake_training = FakeCollection()
+    fake_results = FakeCollection()
 
-    model = OutfitModel(training_collection=fake_training)
+    model = OutfitModel(
+        training_collection=fake_training, results_collection=fake_results
+    )
 
     # mock collection directly
     model.training_collection.insert_one(
@@ -59,8 +68,11 @@ def test_evaluate_outfit():
 
 def test_train_runs():
     fake_training = FakeCollection()
+    fake_results = FakeCollection()
 
-    model = OutfitModel(training_collection=fake_training)
+    model = OutfitModel(
+        training_collection=fake_training, results_collection=fake_results
+    )
 
     model.training_collection.insert_one(
         {"top_color": [255, 0, 0], "bottom_color": [0, 0, 0], "score": 1.0}
@@ -73,8 +85,11 @@ def test_train_runs():
 
 def test_empty_database_raises_error():
     fake_training = FakeCollection()
+    fake_results = FakeCollection()
 
-    model = OutfitModel(training_collection=fake_training)
+    model = OutfitModel(
+        training_collection=fake_training, results_collection=fake_results
+    )
 
     # Make sure DB is empty OR point to a fake collection
     model.training_collection.delete_many({})
